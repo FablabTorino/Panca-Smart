ce66692f0d9dc775d6af2a6d33c54d4708294e16
Paso94 <pasino.francesco@gmail.com>
2019-04-02 00:45:41 +0200

    Update sketch name and clear code

--- software/MKRZERO/MKRZERO.ino
+++ software/MKRZERO/MKRZERO.ino
@@ -12,22 +12,29 @@
 #define DEBUG_G_PIN 4
 #define DEBUG_B_PIN 5
 
-// useful variables 
-const size_t capacity = 2 * JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5); // JSON capacity
+// useful variables
+const size_t capacity = JSON_OBJECT_SIZE(4) + 2*JSON_OBJECT_SIZE(5); // JSON capacity
 String input_string = ""; // a String to hold incoming data
 Adafruit_BME680 bme; // weather sensor with I2C protocol
 RTCZero rtc; // internal real time clock
+DynamicJsonDocument data_json(capacity); // JSON object
+JsonObject status_subjson;
+JsonObject weather_subjson;
 char dir_name[] = "YY-MM-DD";
 char file_name[] = "YY-MM-DD/hh-mm.LOG";
-bool data_backup = false;
+bool read_data = false;
 bool time_is_wrong = false; // on statup the time is always wrong
 
 /*************************************************************************
    setup()
  ************************************************************************/
 void setup() {
+  status_subjson = data_json.createNestedObject("status"); // nested object in JSON object
+  weather_subjson = data_json.createNestedObject("weather"); // nested object in JSON object
+
   // Serial setup ////////////////////////////////////////////////
   Serial.begin(115200);
+  Serial.println();
 
   // Sensors setup /////////////////////////////////////////////// TODO
   debug(bme.begin(), 55); // BME680 via I2C
@@ -46,10 +53,11 @@ void setup() {
   // set custom wrong time
   rtc.setTime(14, 20, 00); // hours, minutes, seconds
   rtc.setDate(01, 04, 19); // day, month, year
+  data_json["wakeup time"] = rtc.getEpoch();
   // set backup routing
   //rtc.setAlarmTime(00, 00, 00);
   rtc.enableAlarm(rtc.MATCH_SS); // Every Minute
-  rtc.attachInterrupt(dataBackup);
+  rtc.attachInterrupt(readData);
   //rtc.standbyMode(); // standby mode to reduce the consume
 }
 
@@ -57,40 +65,21 @@ void setup() {
    loop()
  ************************************************************************/
 void loop() {
-  if (data_backup) {
-    data_backup = false;
+  delay(100); // little dalay to avoid bad Serial response (useful?)
 
-    DynamicJsonDocument doc(capacity); // JSON object
-    // time
-    doc["time"] = rtc.getEpoch();
-    //data
-    JsonObject data = doc.createNestedObject("data"); // nested object in JSON object
-    data["battery"] = 50; // TODO
-    data["solar"] = 12.5; // TODO
-    data["backup battery"] = analogRead(ADC_BATTERY)  * (4.3 / 1023.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 4.3V):
-    // weather
-    debug(bme.performReading(), 56); // update wheather sensors
-    JsonObject weather = doc.createNestedObject("weather"); // nested object in JSON object
-    weather["temperature"] = bme.temperature;
-    weather["humidity"] = bme.humidity;
-    weather["pressure"] = bme.pressure;
-    weather["gas_resistance"] = bme.gas_resistance;
+  if (read_data) {
+    read_data = false;
 
-    // Create a folder to store data, name is YY-MM-DD
-    sprintf(dir_name, "%02d-%02d-%02d",  rtc.getYear(), rtc.getMonth(), rtc.getDay());
-    debug(SD.mkdir(dir_name), 11);
-    // Create a file to store data, name is YY-MM-DD/hh-mm.LOG
-    sprintf(file_name, "%s/%02d-%02d.LOG", dir_name, rtc.getHours(), rtc.getMinutes());
-    File data_file = SD.open(file_name, FILE_WRITE);
-    // if the file is available, write to it:
-    if (debug(data_file, 12)) {
-      // save JSON on file
-      serializeJson(doc, data_file);
-      data_file.close();
+    updateData();
+    if (Serial) {
+      serializeJson(data_json, Serial);
+      Serial.println();
+    }
+    else {
+      backupData();
     }
   }
 
-  delay(100); // little dalay to avoid bad Serial response (useful?)
   if (Serial) {
     input_string = "";
     while (Serial.available()) {
@@ -117,10 +106,40 @@ void loop() {
 /*
    Backup data on SD card, called by interrupt
 */
-void dataBackup() {
-  data_backup = true;
+void readData() {
+  read_data = true;
 }
-
+bool backupData() {
+  // Create a folder to store data, name is YY-MM-DD
+  sprintf(dir_name, "%02d-%02d-%02d",  rtc.getYear(), rtc.getMonth(), rtc.getDay());
+  debug(SD.mkdir(dir_name), 11);
+  // Create a file to store data, name is YY-MM-DD/hh-mm.LOG
+  sprintf(file_name, "%s/%02d-%02d.LOG", dir_name, rtc.getHours(), rtc.getMinutes());
+  File data_file = SD.open(file_name, FILE_WRITE);
+  // if the file is available, write to it:
+  if (debug(data_file, 12)) {
+    // save JSON on file
+    serializeJson(data_json, data_file);
+    data_file.close();
+  }
+}
+/*
+   Update data in json
+*/
+void updateData() {
+  // time
+  data_json["time"] = rtc.getEpoch();
+  // status
+  status_subjson["battery"] = 50; // TODO
+  status_subjson["solar"] = 12.5; // TODO
+  status_subjson["backup battery"] = analogRead(ADC_BATTERY)  * (4.3 / 1023.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 4.3V):
+  // weather
+  debug(bme.performReading(), 56); // update wheather sensor
+  weather_subjson["temperature"] = bme.temperature;
+  weather_subjson["humidity"] = bme.humidity;
+  weather_subjson["pressure"] = bme.pressure;
+  weather_subjson["gas_resistance"] = bme.gas_resistance;
+}
 /*
    debug function | TODO
 */
